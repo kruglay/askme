079ba35c0c576b84b90d6afe52ff4bb788aec3a6
ubuntu12 <kruglay@ubuntu.(none)>
2016-12-13 22:25:21 +0300

    video50

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vadim',
+            username: 'installero',
+            avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+        ),
+        User.new(
+            id: 2,
+            name: 'Misha',
+            username: 'aristofun'
+        )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+
+  end
 
   def show
-    @hello = "Hello, Vladimir"
+    @user = User.new(name: "Vladimir", username: "kruglay",
+            email: "a@am.ru", password: "123",
+                     avatar_url: "http://1avatara.ru/pic/fantasy/fentasy025.jpg")
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('13.12.2016')),
+        Question.new(text: 'Как дела2?', created_at: Date.parse('13.12.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
