a5325ee695708841b49ee85a018e4daf9915d1c4
ubuntu12 <kruglay@ubuntu.(none)>
2016-12-17 15:45:15 +0300

    finish users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :new, :create]
+
   def index
     @users = User.all
   end
@@ -9,17 +11,37 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Данные успешно обновлены"
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @questions = @user.questions.order(created_at: :desc)
 
+    @new_question = @user.questions.build
   end
 
   private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
                                   :name, :username, :avatar_url)
