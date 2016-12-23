16cf84edb50f5fcd39f93453261b37aa436b8780
ubuntu12 <kruglay@ubuntu.(none)>
2016-12-23 14:57:27 +0300

    fix err

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
   def self.color_choices
     [
         ['Серый','slategray', {style: 'color: slategray'}, {class: "form-select-option"}],
-        ['Голубой','blue', {style: 'cocolorlor: blue'}, {class: "form-select-option"}],
+        ['Голубой','blue', {style: 'color: blue'}, {class: "form-select-option"}],
         ['Бирюзовый','turquoise', {style: 'color: turquoise'}, {class: "form-select-option"}],
         ['Ораньжевый','orange', {style: 'color: orange'}, {class: "form-select-option"}],
         ['Зеленый','green', {style: 'color: green'}, {class: "form-select-option"}]
