d2890911f3c106bc1e4b0271a402e29e28e54aca
ubuntu12 <kruglay@ubuntu.(none)>
2017-01-03 23:23:49 +0300

    delete route index for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,6 @@
 class TagsController < ApplicationController
-  def index
-    @questions = Question.all
-  end
-
   def show
     @hashtag = Tag.find(params[:id])
     @questions = @hashtag.question
-    render 'index'
   end
 end
