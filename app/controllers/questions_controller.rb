ef170df0dec22c4eb6bd192d1e6956761593f55c
ubuntu12 <kruglay@ubuntu.(none)>
2017-01-11 22:17:40 +0300

    it works))

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
-      redirect_to user_path(@question.user), alert: 'Заполните текст вопроса'
+      render :edit
     end
   end
 
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,5 @@
-# config/initializers/recaptcha.rb
+#   recaptcha config
 Recaptcha.configure do |config|
-  RECAPTCHA_ASKME_PUBLIC_KEY = "6Lf_UxEUAAAAAN8qXJC4A7veeHqEXdxA-1Lf7_hC"
-  RECAPTCHA_ASKME_PRIVATE_KEY = "6Lf_UxEUAAAAADJjOa6G5sQsNnREmurnV-G9PIVg"
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
+  config.site_key  = ENV['RECAPTCHA_ASKME_PUBLIC_KEY']
+  config.secret_key = ENV['RECAPTCHA_ASKME_PRIVATE_KEY']
 end
