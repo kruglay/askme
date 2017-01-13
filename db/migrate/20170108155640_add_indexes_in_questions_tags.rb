d311bacb081f054b7ea955ab2ba7581fb4329f56
kruglay <kruglay@mail.ru>
2017-01-13 11:59:53 +0300

    add migr index

--- db/migrate/20170108155640_add_indexes_in_questions_tags.rb
+++ db/migrate/20170108155640_add_indexes_in_questions_tags.rb
@@ -3,5 +3,6 @@ class AddIndexesInQuestionsTags < ActiveRecord::Migration
     add_index :questions_tags, :tag_id
     add_index :questions_tags, :question_id
     add_index :questions_tags, [:question_id, :tag_id], unique: true
+
   end
 end
